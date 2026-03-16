4089d92fe452b5373cc51248246979682fa7583b
ejiseok <jiseoklee0502@gmail.com>
2026-03-16 22:42:09 +0900

    기능 추가, 스타일 수정
    - 리셋 기능 추가
    - 컨테이너 테두리 스타일 추가
    - 그리드 아이템 색상 변경

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function createGrid() {
-  for (let i = 0; i < 16 * 16; i++) {
+function createGrid(width) {
+  for (let i = 0; i < width * width; i++) {
     const gridItem = document.createElement("div");
 
     gridItem.classList.add("grid-item");
+    gridItem.style.flexBasis = `calc(${100 / width}%)`;
+    gridItem.style.height = `${960 / width}px`;
 
     gridItem.addEventListener("mouseover", () => {
       gridItem.classList.add("hovering");
@@ -12,7 +14,21 @@ function createGrid() {
   }
 }
 
+function resetGrid() {
+  const gridItems = document.querySelectorAll(".grid-item");
+  gridItems.forEach((item) => {
+    container.removeChild(item);
+  });
+  createGrid(width);
+}
+
 const container = document.querySelector(".container");
+const resetBtn = document.querySelector(".reset-btn");
 
-createGrid();
+let width = 16;
 
+resetBtn.addEventListener("click", () => {
+  resetGrid();
+});
+
+createGrid(width);

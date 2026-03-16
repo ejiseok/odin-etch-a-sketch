a9a341d0e2af98cbe60c88ebda75837851b4d296
ejiseok <jiseoklee0502@gmail.com>
2026-03-16 21:43:28 +0900

    그리드 아이템 호버링 테스트 완료

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < 16 * 16; i++) {
   const gridItem = document.createElement("div");
+
   gridItem.classList.add("grid-item");
+
+  gridItem.addEventListener("mouseover", () => {
+    gridItem.classList.add("hovering");
+  });
+
   container.appendChild(gridItem);
 }

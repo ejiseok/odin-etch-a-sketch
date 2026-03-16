ae712152affe1e46fe1f5e9a4cab13b0d1418762
ejiseok <jiseoklee0502@gmail.com>
2026-03-16 21:55:35 +0900

    스타일 수정과 UI 추가
    - 그리드 컨테이너 사이즈 960px로 수정
    - 타이틀 추가
    - 리셋, 그리드 사이즈 수정 버튼 추가

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+function createGrid() {
+  for (let i = 0; i < 16 * 16; i++) {
+    const gridItem = document.createElement("div");
+
+    gridItem.classList.add("grid-item");
+
+    gridItem.addEventListener("mouseover", () => {
+      gridItem.classList.add("hovering");
+    });
+
+    container.appendChild(gridItem);
+  }
+}
+
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16 * 16; i++) {
-  const gridItem = document.createElement("div");
+createGrid();
 
-  gridItem.classList.add("grid-item");
-
-  gridItem.addEventListener("mouseover", () => {
-    gridItem.classList.add("hovering");
-  });
-
-  container.appendChild(gridItem);
-}
